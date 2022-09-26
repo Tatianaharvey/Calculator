349e64051805d1a7cd62da49e29196c393968c4c
Tatiana <110640811+Tatianaharvey@users.noreply.github.com>
2022-09-26 12:56:26 +0100

    add basic grid

---
+++ script.js
@@ -0,0 +1,35 @@
+const calculator = document.querySelector('.calculator')
+const keys = document.querySelector('.keys')
+const display = document.querySelector('.display')
+
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+      let key = e.target
+      let action = key.dataset.action
+      const keyContent = key.textContent
+      const displayedNum = display.textContent
+     if (!action) {
+        console.log('number key!')
+      } else if (
+
+      
+        action === 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
+        action === 'divide' ) {
+        console.log('operator key!')
+      } else if (action === 'decimal') {
+        console.log('decimal key!')
+      } else if (action === 'clear') {
+        console.log('clear key!')
+      } else {
+        (action === 'calculate')
+        console.log('equal key!')
+      }
+
+    }
+})
+
+
+
