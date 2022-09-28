26cb290004ca8013282396341f28c58eaffa0ef1
Tatiana <110640811+Tatianaharvey@users.noreply.github.com>
2022-09-28 09:25:44 +0100

    add clear button

--- script.js
+++ script.js
@@ -100,10 +100,24 @@ keys.addEventListener('click', e => {
         }
 
     
+      if (action !== 'clear') {
+        const clearButton = calculator.querySelector('[data-action=clear]')
+        clearButton.textContent = 'CE'
+      }
+
       if (action === 'clear') {
-        console.log('clear key!')
+          if (key.textContent === 'AC') {
+            calculator.dataset.firstValue = ''
+            calculator.dataset.modValue = ''
+            calculator.dataset.operator = ''
+            calculator.dataset.previousKeyType = ''
+          } else {
+            key.textContent = 'AC'
+          }
+          display.textContent = 0
         calculator.dataset.previousKeyType = 'clear'
-       }
+      }
+       
 }})
 
 
